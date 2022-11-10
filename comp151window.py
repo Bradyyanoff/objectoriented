ebe07c4c1ed9e82aa5904e21f0bdb033d669e797
Brady Yanoff <113472249+Bradyyanoff@users.noreply.github.com>
2022-11-10 11:57:41 -0500

    Arcade Class sprites visable

--- comp151window.py
+++ comp151window.py
@@ -1,20 +1,25 @@
 import arcade
 
 class Comp151Window(arcade.Window):
-    def __int__(self, width, height):
+    def __init__(self, width, height):
         super().__init__(width, height, "Demo Window")
-        self.player = None
         self.targets = arcade.SpriteList()
+        self.player = None
         self.score = 0
 
     def setup(self):
         self.player = arcade.Sprite("f1-ship1-6.png")
         rock = arcade.Sprite(":resources:images/space_shooter/meteorGrey_med1.png")
         self.targets.append(rock)
+        rock.center_y = 250
+        rock.center_x = 350
+        self.player.center_x = 250
+        self.player.center_y = 250
     def on_update(self, delta_time):
         pass
 
     def on_draw(self):
         arcade.start_render()
-
+        self.player.draw()
+        self.targets.draw()
         arcade.finish_render()
